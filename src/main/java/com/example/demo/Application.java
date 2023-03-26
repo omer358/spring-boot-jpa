ec0c6a4601044a3b42ec6eb80c4c4111c44a5683
omer358 <omermaki358@gmail.com>
2023-03-26 10:41:28 +0200

    implement the faker dependency and get student sorting by

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,50 +24,28 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args ->{
-            Student Maria = new Student("Maria", "john","maria@gmail.com",20);
-            Student huda = new Student("Huda", "Omer","huda@gmail.com",23);
-            Student omer = new Student("Omer", "Khalid", "omer@gmail.com",21);
-            Student ignoredOmer = new Student("Omer", "Taha", "omer.ignored@gmail.com",21);
-            Student omer2 = new Student("Omer", "Mazin", "omer2@gmail.com",23);
-            Student omer3 = new Student("Omer", "Mustafa", "omer3@gmail.com",22);
-            Student khansa = new Student("Khansa", "Jaffar","khansa@gmail.com",30);
-            Student sara = new Student("Sara", "Mustafa","Sara@gmail.com",28);
-            System.out.println("Saving multiple students to the database");
-            studentRepository.saveAll(List.of(Maria,huda,omer,khansa,sara,ignoredOmer));
-
-            System.out.println("find student by Id 1");
-            studentRepository
-                    .findById(1L)
-                    .ifPresentOrElse(
-                            Application::accept,
-                            ()->{
-                                System.out.println("Student not exists");
-            });
-
-            Optional<Student> student = studentRepository.findStudentByEmail("omer@gmail.com");
-            student.ifPresentOrElse(System.out::println,
-                    ()-> System.out.println("There is no student with this email"));
-
-            System.out.println("Find all students by first name");
-            studentRepository.findAllStudentsByFirstName(
-                    "Omer"
-            ).forEach(
-                    System.out::println
+//            generateRandomStudents(studentRepository);
+            Sort firstName = Sort.by(Sort.Direction.DESC, "firstName");
+            studentRepository.findAll(firstName).forEach(
+                    student -> System.out.println(student.getFirstName())
             );
-
-            System.out.println("Find all Student By age greater than 23");
-            studentRepository.findAllStudentsByAgeGreaterThanEqual(23).forEach(
-                    System.out::println
-            );
-            System.out.println("find students by first name");
-            studentRepository.findByFirstName("Omer").forEach(System.out::println);
-
-            System.out.println("Find Student with age greater than or equal to 23");
-            studentRepository.findStudentWithAgeGreaterThan(23).forEach(
-                    System.out::println
-            );
-            System.out.println(studentRepository.deleteStudentById(6L));
         };
+    }
+
+    private static void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i =0;i <= 100;i++){
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@nctr.sd",firstName,lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 60));
+            studentRepository.save(student);
+
+        }
     }
 
 }
